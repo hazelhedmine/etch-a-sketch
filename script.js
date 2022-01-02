0b91ccdf06739fda2be0aaf15501f38082ffd885
hazelhedmine <zelleh15@gmail.com>
2022-01-02 20:16:18 +0800

    added hover effect so that cells change color on mouse over

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-const container = document.querySelector("#container");
+const grid = document.querySelector("#grid");
 
 function makeGrid(row, col) {
-  container.style.setProperty("--grid-rows", row);
-  container.style.setProperty("--grid-cols", col);
+  grid.style.setProperty("--grid-rows", row);
+  grid.style.setProperty("--grid-cols", col);
   for (let c = 0; c < row * col; c++) {
-    let cell = document.createElement("div");
+    const cell = document.createElement("div");
     cell.classList.add("grid-item");
+    cell.addEventListener("mouseover", changeGridToBlack);
     //cell.textContent = c + 1;
-    container.appendChild(cell);
+    grid.appendChild(cell);
   }
   console.log(`Grid of size ${row} has been created!`);
 }
 
 makeGrid(16, 16);
+
+function changeGridToBlack(e) {
+  //Use event delegation
+  e.target.style.backgroundColor = "black";
+}
